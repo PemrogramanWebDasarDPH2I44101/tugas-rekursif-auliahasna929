a2e069697c1c59433df7542d710e329b92bffbb8
auliahasna929 <42725478+auliahasna929@users.noreply.github.com>
2018-09-10 19:35:53 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,35 @@
 <?php
-
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+function rekursif($a) {
+	$a --;
+	if ($a > 0) rekursif($a);
+	echo"*";
 }
-rekursif(5);
+function rekursif1($a) {
+	rekursif($a);
+	echo "<br>";
+	$a --;
+	if ($a>0)
+		rekursif1($a);
+}
+rekursif1(5);
+
+//no2 
+
+function recursive($a,$c) {
+  global $col;
+  recursive1($col, $a, $c);
+  echo "<br>";
+  if (++$a < $col) {
+    recursive($a, $c);
+  }
+}
+function recursive1($a, $h, $b) {
+  global $col; 
+  if ($a > $col - $h - 1) {
+    echo "$b";
+    recursive1($a - 1, $h, $b +1);
+  }
+}
+$col = 5;
+recursive(0,1);
 ?>
